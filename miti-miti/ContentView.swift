ee6c3bc9c7528fd44aec7b5f156dc18de451740d
César Cruz <cecruzmo@gmail.com>
2024-07-03 00:24:56 -0500

    feat: initial project 1

---
+++ miti-miti/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  miti-miti
+//
+//  Created by cecruzmo on 2/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ miti-miti/MitiMitiApp.swift
@@ -0,0 +1,17 @@
+//
+//  miti_mitiApp.swift
+//  miti-miti
+//
+//  Created by cecruzmo on 2/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MitiMitiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
